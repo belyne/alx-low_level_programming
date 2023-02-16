d90d28c3c7f189eba0985aeb44676efae056d3e6
belyne <nzabonamarie@gmail.com>
2023-02-16 13:51:17 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	writes(1"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
 
