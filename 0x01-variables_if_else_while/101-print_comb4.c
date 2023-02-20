3c71f23e3cc5ce90b145dfe9bf14d2690e2b8669
belyne <nzabonamarie@gmail.com>
2023-02-20 01:25:30 -0800

    task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,25 +11,24 @@ int main(void)
 	int j;
 	int k;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = '0'; i < '9'; i++)
 	{
-		for (j = 1 ; j < 10 ; j++)
-		{
-			for (k = 2 ; k < 10 ; k++)
-			{
-				if (i < j && j < k)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					if (i + j + k != 24)
-					{
-						putchar(',');
-					        putchar(' ');
-					}
-				}
-			}
-		}
+	for (j = i + 1; j <= '9'; j++)
+	{
+	for (k = j + 1; k <= '9'; k++)
+	{
+	if ((j != i) != k)
+	{
+	putchar(i);
+	putchar(j);
+	putchar(k);
+	if (i == '7' && j == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
