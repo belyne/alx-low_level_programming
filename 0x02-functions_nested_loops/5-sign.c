ab824d30ec13234a81eadb9906b526fb6cee5fda
belyne <nzabonamarie@gmail.com>
2023-02-21 13:23:41 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,23 +3,25 @@
 /**
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
- * Return: 1 for positive num, -1 for negative num and zero for anything else
+ * Return: 1 if number is greater than zero
+ * 0 oooof number is zero
+ * -1 if number is less than zero
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar(3);
+	_putchar('+');
 	return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-	_putchar(45);
-	return (-1);
+	_putchar('0');
+	return (0);
 	}
 	else
 	{
-	_putchar(48);
-	return (0);
+	_putchar('-');
+	return (-1);
 	}
 }
