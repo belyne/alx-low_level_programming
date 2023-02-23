c7cf24b3c805c6093f966c15a3abeaa17998c0a0
belyne <nzabonamarie@gmail.com>
2023-02-23 13:41:31 -0800

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - Checks for a digit 0 - 9
+ *@a: a - variable
+ *@b: b - variable
+ *Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
