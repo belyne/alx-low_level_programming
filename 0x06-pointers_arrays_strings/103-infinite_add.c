6729dec70cdf38083f856572c1dd0f148167a59f
belyne <nzabonamarie@gmail.com>
2023-03-02 12:36:59 -0800

    Addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -17,7 +17,6 @@ void rev_string(char *n)
 		i++;
 	}
 	i--;
-
 	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
@@ -27,14 +26,13 @@ void rev_string(char *n)
 }
 
 /**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
+ *infinie_add - add 2 numbers together
+ *@n1: text representation of 1st number to add
+ *@n2: text representation of 2nd number to add
+ *@r: pointer to buffer
+ *@size_r: buffer size
+ *Return: pointer to calling function
  */
-
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
@@ -43,6 +41,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (*(n1 + i) != '\0')
 		i++;
+	while (*(n2 + j) != '\0')
+		j++;
 	i--;
 	j--;
 	if (j >= size_r || i >= size_r)
@@ -50,18 +50,18 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
-			val1 = 0;
+		val1 = 0;
 		else
 			val1 = *(n1 + i) - '0';
 		if (j < 0)
-			val2 = '0';
-		else 
+			val2 = 0;
+		else
 			val2 = *(n2 + j) - '0';
 		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
 			overflow = 1;
-		else 
-			overflow = '0';
+		else
+			overflow = 0;
 		if (digits >= (size_r - 1))
 			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -35,6 +35,17 @@ int main(void)
 	{
 		printf("%s + %s = %s\n", n, m, res);
 	}
+	n = "999999999";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
 	res = infinite_add(n, m, r3, 11);
 	if (res == 0)
 	{
