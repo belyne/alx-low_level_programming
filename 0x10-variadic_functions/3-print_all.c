1e27b0769b6e8b3ad87017e3f144eb250d300e67
belyne <nzabonamarie@gmail.com>
2023-03-24 09:54:47 -0700

    0x10project

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,41 +8,48 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	char *str, *sep = "";
-	va_list list;
-	va_start(list, format);
+        unsigned int i = 0, j, c = 0;
+        char *str = "";
+        const char t_arg[] = "cifs";
+        va_list list;
+        va_start(list, format);
 
-	if (format)
-	{
-		while (format[i])
-		{
-			 switch (format[i])
-			 {
-				 case 'c':
-					  printf("%s%c", sep, va_arg(list, int));
-					  break;
-				 case 'i':
-					  printf("%s%d", sep, va_arg(list, int));
-					  break;
-				 case 'f':
-					  printf("%s%f", sep, va_arg(list, double));
-					  break;
-				 case 's':
-					  str = va_arg(list, char *);
-					   if (!str)
-						   str = "(nil)";
-					    printf("%s%s", sep, str);
-					    break;
-				 default:
-					    i++;
-					    continue;
-			 }
-			 sep = ",";
-			 i++;
-		}
-	}
-	printf("\n");
-	va_end(list);
+        while (format && format[i])
+        {
+                j = 0;
+                while (t_arg[j])
+                {
+                         if (format[i] == t_arg[j] && c)
+                         {
+                                 printf(" , ");
+                                 break;
+                         }
+                         j++;
+                }
+                switch (format[i])
+                {
+                        case 'c':
+                                printf("%c", va_arg(list, int)), c = 1;
+                                break;
+                        case 'i':
+                                printf("%d", va_arg(list, int)), c = 1;
+                                break;
+                        case 'f':
+                                printf("%f", va_arg(list, double)), c = 1;
+                                break;
+                        case 's':
+                                str = va_arg(list, char *), c = 1;
+                                if (!str)
+                                {
+                                        printf("(nil)");
+                                        break;
+                                }
+                                printf("%s", str);
+                                break;
+                }
+                i++;
+        }
+        printf("\n");
+        va_end(list);
 }
 
