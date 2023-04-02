b7fdecd962acfe15ea6b0e660dc59303bccd996b
belyne <122968087+belyne@users.noreply.github.com>
2023-04-02 22:01:07 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,36 +11,38 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	int i;
+	
+	if (n >= 0 && n <= 98)
 	{
-	for (; n <= 98; n++)
+	for (i = n; i <= 98; i++)
 	{
-	if (n == 98)
-	{
-	printf("%d\n", n);
+	printf("%d", i);
+	if (i != 98)
+	printf(", ");
+	}
 	printf("\n");
-	break;
 	}
-	else
+	else if (n < 0)
 	{
-	printf("%d, ", n);
+	for (i = n; i <= 98; i++)
+	{
+	printf("%d", i);
+	if (i != 98)
+	printf(", ");
 	}
-	}
-	}
-	else
-	{
-	for (; n >= 98; n--)
-	{
-	if (n == 98)
-	{
-	printf("%d", n);
 	printf("\n");
-	break;
 	}
-	else
+	else if (n > 98)
 	{
-	printf("%d, ", n);
+	for (i = n; i >= 98; i--)
+	{
+	printf("%d", i);
+	if (i != 98)
+	printf(", ");
 	}
+	printf("\n");
 	}
+	else if (n == 0)
+	printf(", ");
 	}
-}
