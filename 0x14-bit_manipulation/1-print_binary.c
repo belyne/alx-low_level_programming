811f00da105c3ca6bd6ca244c887606828076c2c
belyne <nzabonamarie@gmail.com>
2023-04-10 06:58:10 -0700

    Task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary represenation
+ * of number
+ * @n: number in binary units
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int i, n_cnt = 0;
+
+	unsigned long int now;
+
+	for (i = 63; i >= 0; i--)
+	{
+		now = n >> i;
+		if (now & 1)
+		{
+			_putchar('1');
+			n_cnt++;
+		}
+		else if (n_cnt)
+			_putchar('0');
+	}
+	if (!n_cnt)
+		_putchar('0');
+}
--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes a character c to stdout
