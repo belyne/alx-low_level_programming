69fca90070df224dd3bff25f1cf68ad8d0e7fb65
belyne <nzabonamarie@gmail.com>
2023-04-10 09:09:30 -0700

    Task3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,8 +10,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index >= (sizeof(unsigned long int) *8))
 		return (-1);
-	*n = ((1 << index) | *n);
+
+	*n ^= (1 << index);
+
 	return (1);
 }
