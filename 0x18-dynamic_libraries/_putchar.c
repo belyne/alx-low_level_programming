ca54693fd15e25ac8015d714344f1da9b4e3bb9e
belyne <nzabonamarie@gmail.com>
2023-06-19 02:10:16 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - writes a character to the standard output
+ * @c: the character to be written
+ *
+ * Return: On success, returns the character written.
+ * On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
